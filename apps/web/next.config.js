c30fc32089c3bc632e0523681d80cde06a1c3b79
현혜진 <hyeonhyejin@hyeonhyejin-ui-MacBookPro.local>
2024-12-17 15:55:04 +0900

    init project

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+const withVanillaExtract = createVanillaExtractPlugin();
+
 const nextConfig = {};
 
-export default nextConfig;
+module.exports = withVanillaExtract(nextConfig);
